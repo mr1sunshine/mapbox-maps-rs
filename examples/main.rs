38621e6d1b8a0cafad046902eb30f11f1a07683e
Aleksandr Ovchinnikov <mail@mr1sunshine.com>
2020-10-21 00:33:32 +0300

    Add window to example app

--- examples/main.rs
+++ examples/main.rs
@@ -1,13 +1,45 @@
-use mapbox_maps::{Map, Config};
-use std::env;
 use eyre::Result;
+use mapbox_maps::{Config, Map};
+use std::env;
+use winit::{
+    event::{ElementState, Event, KeyboardInput, VirtualKeyCode, WindowEvent},
+    event_loop::{ControlFlow, EventLoop},
+    window::WindowBuilder,
+};
 
 #[tokio::main]
 async fn main() -> Result<()> {
-    let token = env::var("MAPBOX_ACCESS_TOKEN").expect("Provide MAPBOX_ACCESS_TOKEN as env variable.");
+    let token =
+        env::var("MAPBOX_ACCESS_TOKEN").expect("Provide MAPBOX_ACCESS_TOKEN as env variable.");
     let mut map = Map::new(Config::new(&token))?;
 
     map.load_style("mapbox/streets-v11").await?;
 
-    Ok(())
+    let event_loop = EventLoop::new();
+    let window = WindowBuilder::new().build(&event_loop)?;
+
+    event_loop.run(move |event, _, control_flow| {
+        *control_flow = ControlFlow::Wait;
+
+        match event {
+            Event::WindowEvent { event, window_id } if window_id == window.id() => match event {
+                WindowEvent::CloseRequested => *control_flow = ControlFlow::Exit,
+                WindowEvent::KeyboardInput { input, .. } => {
+                    if let KeyboardInput {
+                        state: ElementState::Pressed,
+                        virtual_keycode: Some(VirtualKeyCode::Escape),
+                        ..
+                    } = input
+                    {
+                        *control_flow = ControlFlow::Exit
+                    }
+                }
+                _ => {}
+            },
+            Event::RedrawRequested(_) => {
+                println!("redraw requested");
+            }
+            _ => (),
+        }
+    });
 }
